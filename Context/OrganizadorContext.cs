d89a722b2400f716cebbc943e325439b6f67018e
MatheusGress <matheusaugustogress@gmail.com>
2024-06-18 17:59:38 -0300

    fix: modify OrganizadorContext.cs

--- Context/OrganizadorContext.cs
+++ Context/OrganizadorContext.cs
@@ -7,9 +7,26 @@ namespace TrilhaApiDesafio.Context
     {
         public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
         {
-            
-        }
 
+        }
         public DbSet<Tarefa> Tarefas { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tarefa>(entity =>
+            {
+                entity.ToTable("Tarefas");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Titulo)
+                      .IsRequired()
+                      .HasMaxLength(255);
+                entity.Property(e => e.Descricao)
+                      .HasMaxLength(1000);
+                entity.Property(e => e.Data)
+                      .IsRequired();
+                entity.Property(e => e.Status)
+                      .IsRequired()
+                      .HasConversion<string>();
+            });
+        }
     }
 }
